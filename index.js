1474190670e636a2ff0340b949a927f905bb325b
Gideon Crawley <gscrawley@gmail.com>
2021-05-03 14:38:40 -0700

    finally figured out my error and included name inside the message, then it worked

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const sendBtn = document.querySelector('#send')
 const messages = document.querySelector('#messages')
 const messageInput = document.querySelector('#message-input')
+const nameInput = document.querySelector('#name-input')
 
 let ws
 
 // Display messages from the websocket
-function showMessage(message) {
-    messages.innerHTML += `${message}\n\n` // display the message
-    messages.scrollTop = messages.scrollHeight // scroll to the top
-    messageInput.value = '' // clear the input field
+function showMessage(data) {
+  messages.innerHTML += `<li>${data.name}:${data.message} ${data.date}\n\n</li>` // display the message
+  messages.style.color = "orange";
+  // messages.style.border = "1px solid #FF0000"
+  messages.scrollTop = messages.scrollHeight // scroll to the top
+  messageInput.value = "" // clear the input field
   }
   
   function init() {
-    // Clean up before restarting a websocket connection
     if (ws) {
       ws.onerror = ws.onopen = ws.onclose = null;
       ws.close();
@@ -22,27 +24,27 @@ function showMessage(message) {
     ws = new WebSocket('ws://localhost:6969')
   
     // Handle the connection when it opens
-    ws.onopen = () => console.log('!Connection opened!')
-  
+    ws.onopen = () => {
+      console.log('!Connection opened!')
+    }
     // handle a message event
-    ws.onmessage = (e) => showMessage(e.data)
+    ws.onmessage = (e) => showMessage(JSON.parse(e.data))
     
     // Handle a close event
-    ws.onclose = () => ws = null
-  
+    ws.onclose = () => {
+      ws = null
   }
-  
-  // Handle button clicks
+}
   sendBtn.onclick = function () {
-    // Send a message
     if (!ws) {
         showMessage("No WebSocket connection :(");
         return;
-      }
-      
-    ws.send(messageInput.value);
-    showMessage(messageInput.value);
-    
-  }
+        }
+        const date = new Date(Date.now() * 1000)
+        const data = { message: messageInput.value, name: nameInput.value, date: date }
+        ws.send(JSON.stringify(data))
+        console.log(data)
+        showMessage(data)
+    }
   
   init();
